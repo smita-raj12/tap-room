0e1912ac04241d500133f30ab9e0734535a34a86
smita <smita.raj12@gmail.com>
2022-01-13 13:17:45 -0800

    create components directory

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import TapControl from "./TapControl";
+function App(){
+ 
+  return (
+    <React.Fragment>
+      <Header />
+      <TapControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
